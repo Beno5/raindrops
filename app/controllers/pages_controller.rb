b31e0426f3ea3e54623d23dd6a59fb48478f0972
benjamin fejzagic <benjamin.fejzagic@hotmail.com>
2023-09-20 23:03:08 +0200

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,53 +1,45 @@
 class PagesController < ApplicationController
   def home
     @results = (1..24).map { |number| [number, raindrops(number)] }
-    @generated_result = [params[:id], raindrops(params[:id].to_i)]
+    @generated_result = [params[:id], raindrops(params[:id].to_i)] unless params[:id] == '0'
   end
 
   def raindrops(number)
     sounds = {
-      2 => "pling",
-      3 => "plang",
-      5 => "plong",
-      17 => "tshäng"
+      2 => 'pling',
+      3 => 'plang',
+      5 => 'plong',
+      17 => 'tshäng'
     }
-  
+
     result = []
-  
+
     sounds.each do |divisor, sound|
       count = 0
       while number % divisor == 0
         number /= divisor
         count += 1
       end
-  
+
       while count >= 4
         result << adjust_sound(sound, 4)
         count -= 4
       end
-  
-      if count.positive?
-        result.unshift(adjust_sound(sound, count))  
-      end
-      
+      result.unshift(adjust_sound(sound, count)) if count.positive?
     end
-  
-    result.empty? ? "BLOB" : result.join(', ')
+
+    result.empty? ? 'BLOB' : result.join(', ')
   end
-  
-  
-  
 
   def adjust_sound(sound, count)
-
     if count == 4
-      return "*#{sound.upcase}* #{sound.downcase}"
+      "*#{sound.upcase}* #{sound.downcase}"
     elsif count == 3
-      return "*#{sound.upcase}*"
+      "*#{sound.upcase}*"
     elsif count == 2
-      return sound.upcase
+      sound.upcase
     else
-      return sound
+      sound
     end
   end
 end
